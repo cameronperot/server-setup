454e9b82cccf1f6da26c709907fa83cdf5c520a2
Cameron Perot <cameronperot@users.noreply.github.com>
2020-01-31 16:09:25 -0600

    Added main server setup files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Server Setup
+
+## Usage
+
+Before running the script you will want to change the `new_user`, `ssh_key1`, and `ssh_key2` variables in `server-setup.sh`. to your desired values.
+
+```bash
+git clone https://github.com/cameronperot/server-setup.git
+cd server-setup
+./server-setup.sh <IPTABLES_RULESET> <DEV>
+```
+
+Where:
+* `<IPTABLES_RULESET>` is one of `[standard, vpn]` depending on which iptables ruleset you would like to use.
+* `<DEV>` is `true` if you wish to install additional development packages, otherwise leave blank.
+
---
+++ server-setup.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+set -e
+
+# Ensure script is run as root
+if [ "$USER" != 'root' ]; then
+	echo "You must run this script as root!"
+	exit 1
+fi
+
+# Check that a valid ipruleset was provided
+if [[ !("$1" == "standard" || "$1" == "vpn") ]]; then
+	echo "Invalid iptables ruleset type provided (arg 1), valid options are: [standard, vpn]"
+	exit 1
+fi
+
+# Directory where config files are located
+setup_dir=$PWD
+
+# Username, SSH keys, and location of user's home directory
+new_user=cameron
+ssh_key1="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKIukxLRRwpbgDxqcsdRY77i7T+Ptsrs8J9tfNrWncHK"
+ssh_key2="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8Nsh7r7SoYZD2JZolMZCJVT9O6OkKlQuQl27YlqQVy"
+home_dir=/home/$new_user
+
+# Create the new user and add them to the sudo group
+adduser --gecos "" $new_user
+usermod -a -G sudo $new_user
+
+# Aliases for the new user
+echo "alias ls='ls -lahF --color=always'" >> $home_dir/.bash_aliases
+sudo chown $new_user:$new_user $home_dir/.bash_aliases
+
+# Set up the new user's ~/.ssh directory and authorized_keys
+mkdir $home_dir/.ssh
+chmod 700 $home_dir/.ssh
+echo $ssh_key1 >> $home_dir/.ssh/authorized_keys
+echo $ssh_key2 >> $home_dir/.ssh/authorized_keys
+chmod 600 $home_dir/.ssh/authorized_keys
+chown -R $new_user:$new_user $home_dir/.ssh
+
+# Update and install packages
+apt-get update
+apt-get -y upgrade
+apt-get -y install \
+	aptitude \
+	git \
+	rsync \
+	tmux \
+	sudo \
+	vim \
+	nload \
+	iptables-persistent \
+	htop \
+	curl \
+	ca-certificates \
+	unattended-upgrades \
+	apt-listchanges
+
+# Install packages for dev server
+if [ "$2" == "true" ]; then
+	apt-get -y install \
+		libmariadbclient-dev \
+		python3-dev \
+		libsasl2-dev \
+		build-essential \
+		postgresql \
+		postgresql-contrib
+fi
+
+# Copy server setup files from the root directory to the users home directory
+cp -r $setup_dir/python $home_dir
+chown -R $new_user:$new_user $home_dir/python
+
+# Configure unattended upgrades
+dpkg-reconfigure -plow unattended-upgrades
+
+# Generate server SSH keys (ed25519 and 4096-bit RSA)
+cd /etc/ssh
+rm ssh_host_*key*
+ssh-keygen -t ed25519 -f ssh_host_ed25519_key < /dev/null
+ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key < /dev/null
+
+# Edit the moduli file to remove small primes
+awk '$5 > 2000' /etc/ssh/moduli > "${HOME}/moduli"
+wc -l "${HOME}/moduli"
+mv "${HOME}/moduli" /etc/ssh/moduli
+
+# Replace sshd_config and ssh_config files
+cp $setup_dir/etc/issue.net /etc/issue.net
+cp $setup_dir/etc/ssh/*_config /etc/ssh/
+chmod 644 /etc/ssh/*_config
+
+# Disable ipv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+# Replace iptables rules and restart iptables/sshd
+touch /option.netfilter
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --set arptables /usr/sbin/arptables-legacy
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+
+cp $setup_dir/etc/iptables/$1/* /etc/iptables/
+chmod 644 /etc/iptables/rules.v*
+iptables-restore /etc/iptables/rules.v4
+ip6tables-restore /etc/iptables/rules.v6
+systemctl restart sshd
