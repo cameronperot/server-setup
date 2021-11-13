1f18aa9d7116e6619c4a07b1992f47dc54e5c0ba
Cameron Perot <cameronperot@users.noreply.github.com>
2021-11-13 01:41:02 +0100

    Updated setup script

--- server-setup.sh
+++ server-setup.sh
@@ -1,68 +1,68 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P )"
+
 # Ensure script is run as root
 if [ "$USER" != "root" ]; then
-	echo "You must run this script as root!"
-	exit 1
+    echo "You must run this script as root!"
+    exit 1
 fi
 
 # Check that a valid ipruleset was provided
 if [[ ! ("$1" == "standard" || "$1" == "vpn") ]]; then
-	echo "Invalid iptables ruleset type provided (arg 1), valid options are: [standard, vpn]"
-	exit 1
+    echo "Invalid iptables ruleset type provided (arg 1), valid options are: [standard, vpn]"
+    exit 1
 fi
 
-# Directory where config files are located
-setup_dir=$PWD
-
 # Username, SSH keys, and location of user's home directory
-new_user=cameron
-ssh_key1="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhUSbmv/3Q0vMYLdaNLGcBwvNpGKjLSeBdf/z+JETO1"
-home_dir=/home/$new_user
+echo "What would you like the username to be?"
+read NEW_USER
+SSH_KEY1="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhUSbmv/3Q0vMYLdaNLGcBwvNpGKjLSeBdf/z+JETO1"
+HOME_DIR="/home/$NEW_USER"
 
 # Create the new user and add them to the sudo group
-adduser --gecos "" $new_user
-usermod -a -G sudo $new_user
+adduser --gecos "" $NEW_USER
+usermod -a -G sudo $NEW_USER
 
 # Aliases for the new user
-echo "alias ls='ls -lahF --color=always'" >> $home_dir/.bash_aliases
-sudo chown $new_user:$new_user $home_dir/.bash_aliases
+echo "alias ls='ls -lahF --color=always'" >> "$HOME_DIR/.bash_aliases"
+chown "$NEW_USER:$NEW_USER" "$HOME_DIR"/.bash_aliases
 
 # Set up the new user's ~/.ssh directory and authorized_keys
-mkdir $home_dir/.ssh
-chmod 700 $home_dir/.ssh
-echo $ssh_key1 >> $home_dir/.ssh/authorized_keys
-chmod 600 $home_dir/.ssh/authorized_keys
-chown -R $new_user:$new_user $home_dir/.ssh
+mkdir "$HOME_DIR/.ssh"
+chmod 700 "$HOME_DIR/.ssh"
+echo "$SSH_KEY1" >> "$HOME_DIR/.ssh/authorized_keys"
+chmod 600 "$HOME_DIR/.ssh/authorized_keys"
+chown -R "$NEW_USER:$NEW_USER" "$HOME_DIR/.ssh"
 
 # Update and install packages
 apt-get update
 apt-get -y upgrade
 apt-get -y install \
-	aptitude \
-	git \
-	rsync \
-	tmux \
-	sudo \
-	vim \
-	nload \
-	iptables-persistent \
-	htop \
-	curl \
-	ca-certificates \
-	unattended-upgrades \
-	apt-listchanges
+    aptitude \
+    git \
+    rsync \
+    tmux \
+    sudo \
+    vim \
+    nload \
+    iptables-persistent \
+    htop \
+    curl \
+    ca-certificates \
+    unattended-upgrades \
+    apt-listchanges
 
 # Install extra dev packages
-if [ "$2" == "true" ]; then
-	apt-get -y install \
-		libmariadbclient-dev \
-		python3-dev \
-		libsasl2-dev \
-		build-essential \
-		postgresql \
-		postgresql-contrib
+if [ "${2:-false}" == "true" ]; then
+    apt-get -y install \
+        libmariadbclient-dev \
+        python3-dev \
+        libsasl2-dev \
+        build-essential \
+        postgresql \
+        postgresql-contrib
 fi
 
 # Configure unattended upgrades
@@ -80,8 +80,8 @@ wc -l "${HOME}/moduli"
 mv "${HOME}/moduli" /etc/ssh/moduli
 
 # Replace sshd_config and ssh_config files
-cp $setup_dir/etc/issue.net /etc/issue.net
-cp $setup_dir/etc/ssh/*_config /etc/ssh/
+cp "$DIR"/etc/issue.net /etc/issue.net
+cp "$DIR"/etc/ssh/*_config /etc/ssh/
 chmod 644 /etc/ssh/*_config
 
 # Disable ipv6
@@ -95,10 +95,10 @@ EOT
 touch /option.netfilter
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-update-alternatives --set arptables /usr/sbin/arptables-legacy
-update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+#update-alternatives --set arptables /usr/sbin/arptables-legacy
+#update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 
-cp $setup_dir/etc/iptables/$1/* /etc/iptables/
+rsync -a "$DIR/etc/iptables/$1/" /etc/iptables/
 chmod 644 /etc/iptables/rules.v*
 iptables-restore /etc/iptables/rules.v4
 ip6tables-restore /etc/iptables/rules.v6
